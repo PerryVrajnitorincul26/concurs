7c98888a42eb14be966ef6533c4be482246e6a51
namelessman26@gmail.com <perry@PerryTheWizz.ro>
2020-01-29 12:03:53 +0200

    CServ commit C S

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
 from http.server import BaseHTTPRequestHandler
 import os 
 class Server(BaseHTTPRequestHandler):
+      
   def do_HEAD(self):
+    try:
+      pth=os.getcwd()
+      f=open(pth+self.path)
+      self.send_response(200,"OK")
+      type=self.path.rfind(".")
+      self.send_header("Content-type","text/"+self.path[type:])
+    except IOError:
+      self.send_header("Content-type", "text/html")
+      self.end_headers()
+      self.wfile.write(bytes("404 not found","UTF-8"))
+      self.send_error(404,"NOT FOUND")
+
     return
   def do_POST(self):
     return
@@ -25,6 +38,30 @@ class Server(BaseHTTPRequestHandler):
         f=open(pth,"r+")
         self.wfile.write(bytes(f.read(),"UTF-8"))
         f.close()
+      elif (self.path.endswith(".js")):
+        self.send_response(200,"MESSEGE TEST")
+        self.send_header("Content-type", "text/javascript")
+        self.end_headers()
+        pth=os.path.abspath(os.getcwd())
+        pth=pth+self.path
+        f=open(pth,"r+")
+        self.wfile.write(bytes(f.read(),"UTF-8"))
+        f.close()
+      elif (self.path.endswith(".jpg") or self.path.endswith(".png")):
+        pth=os.path.abspath(os.getcwd())
+        pth=pth+self.path
+        f = open(pth , 'rb')
+        self.send_response(200)
+        self.send_header('Content-type',        'image/png')
+        self.end_headers()
+        self.wfile.write(f.read())
+        f.close()
+        return
+      else:
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("415 unsupported media extension","UTF-8"))
+        self.send_error(415,"BAD REQUEST")
     except IOError:
       self.send_header("Content-type", "text/html")
       self.end_headers()
